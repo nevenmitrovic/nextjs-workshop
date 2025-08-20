47d735a2e85e2ad657efa92908a3073ba637e70b
neven <nevenmitrovic4@gmail.com>
2025-08-20 11:07:52 +0200

    add meals error component

---
+++ foodies/app/meals/error.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function MealsError() {
+	return (
+		<main className='error'>
+			<h1>An error occured!</h1>
+			<p>Fetching meals failed. Please try again.</p>
+		</main>
+	)
+}
