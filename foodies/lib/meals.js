ccda7f8aea46823e35da5e3dba786bd116d6c320
neven <nevenmitrovic4@gmail.com>
2025-08-20 10:46:06 +0200

    add meals loading component

--- foodies/lib/meals.js
+++ foodies/lib/meals.js
@@ -3,6 +3,5 @@ import sql from 'better-sqlite3'
 const db = sql('meals.db')
 
 export default async function getMeals() {
-	new Promise((resolve) => setTimeout(resolve, 2000)) // for loading state
 	return db.prepare('SELECT * FROM meals').all()
 }
