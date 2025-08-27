c2d873b62f0d4b81094aa9671bcc85257f819e4c
neven <nevenmitrovic4@gmail.com>
2025-08-27 16:30:10 +0200

    chore: separate the create post action in the actions folder

---
+++ social/actions/post.js
@@ -0,0 +1,37 @@
+import { redirect } from 'next/navigation'
+
+import { storePost } from '@/lib/posts'
+
+export async function createPost(prevState, formData) {
+	'use server'
+	const title = formData.get('title')
+	const image = formData.get('image')
+	const content = formData.get('content')
+
+	const errors = []
+
+	if (!title || title.trim().length === 0) {
+		errors.push('Title is required.')
+	}
+
+	if (!image || image.size === 0) {
+		errors.push('Image is required.')
+	}
+
+	if (!content || content.trim().length === 0) {
+		errors.push('Content is required.')
+	}
+
+	if (errors.length > 0) {
+		return { errors }
+	}
+
+	await storePost({
+		imageUrl: '',
+		title,
+		content,
+		userId: 1,
+	})
+
+	redirect('/feed')
+}
--- social/app/new-post/page.js
+++ social/app/new-post/page.js
@@ -1,43 +1,7 @@
-import { redirect } from 'next/navigation'
-
-import { storePost } from '@/lib/posts'
+import { createPost } from '@/actions/post'
 import PostForm from '@/components/post-form'
 
 export default function NewPostPage() {
-	async function createPost(prevState, formData) {
-		'use server'
-		const title = formData.get('title')
-		const image = formData.get('image')
-		const content = formData.get('content')
-
-		const errors = []
-
-		if (!title || title.trim().length === 0) {
-			errors.push('Title is required.')
-		}
-
-		if (!image || image.size === 0) {
-			errors.push('Image is required.')
-		}
-
-		if (!content || content.trim().length === 0) {
-			errors.push('Content is required.')
-		}
-
-		if (errors.length > 0) {
-			return { errors }
-		}
-
-		await storePost({
-			imageUrl: '',
-			title,
-			content,
-			userId: 1,
-		})
-
-		redirect('/feed')
-	}
-
 	return (
 		<>
 			<h1>Create a new post</h1>
