eb391d5d84f347c2b329bded742b54ff1e86b3dd
neven <nevenmitrovic4@gmail.com>
2025-08-28 11:51:11 +0200

    add next cache

--- caching-project/app/messages/page.js
+++ caching-project/app/messages/page.js
@@ -6,14 +6,14 @@ import { getMessages } from '@/lib/messages'
 // export const revalidate = 5 // number of seconds
 // export const dynamic = 'force-dynamic' // always rerender and no-cache
 
-export default function MessagesPage() {
+export default async function MessagesPage() {
 	// unstable_noStore()
 	// const response = await fetch('http://localhost:8080/messages', {
 	// 	next: { tags: ['msg'] },
 	// })
 	// const messages = await response.json()
 
-	const messages = getMessages()
+	const messages = await getMessages()
 
 	if (!messages || messages.length === 0) {
 		return <p>No messages found</p>
