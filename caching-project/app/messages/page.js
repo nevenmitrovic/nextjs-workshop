b65b9cc6cdbc12b5af3ebf6e7d7b6f8192566dea
neven <nevenmitrovic4@gmail.com>
2025-08-28 11:44:23 +0200

    add request memoization for custom data source

--- caching-project/app/messages/page.js
+++ caching-project/app/messages/page.js
@@ -1,16 +1,19 @@
 import { unstable_noStore } from 'next/cache'
 
 import Messages from '@/components/messages'
+import { getMessages } from '@/lib/messages'
 
 // export const revalidate = 5 // number of seconds
 // export const dynamic = 'force-dynamic' // always rerender and no-cache
 
-export default async function MessagesPage() {
+export default function MessagesPage() {
 	// unstable_noStore()
-	const response = await fetch('http://localhost:8080/messages', {
-		next: { tags: ['msg'] },
-	})
-	const messages = await response.json()
+	// const response = await fetch('http://localhost:8080/messages', {
+	// 	next: { tags: ['msg'] },
+	// })
+	// const messages = await response.json()
+
+	const messages = getMessages()
 
 	if (!messages || messages.length === 0) {
 		return <p>No messages found</p>
