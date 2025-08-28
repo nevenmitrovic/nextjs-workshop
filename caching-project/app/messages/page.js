9a251f1914604c43d29b01c63e7ec7a2fa66b859
neven <nevenmitrovic4@gmail.com>
2025-08-28 11:28:25 +0200

    add tag in fetch config for messages

--- caching-project/app/messages/page.js
+++ caching-project/app/messages/page.js
@@ -1,10 +1,14 @@
+import { unstable_noStore } from 'next/cache'
+
 import Messages from '@/components/messages'
 
+// export const revalidate = 5 // number of seconds
+// export const dynamic = 'force-dynamic' // always rerender and no-cache
+
 export default async function MessagesPage() {
+	// unstable_noStore()
 	const response = await fetch('http://localhost:8080/messages', {
-		next: {
-			revalidate: 5,
-		},
+		next: { tags: ['msg'] },
 	})
 	const messages = await response.json()
 
