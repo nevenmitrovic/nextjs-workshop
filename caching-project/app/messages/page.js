6e2c2cd0737a58a7b994d3975858908894392a5c
neven <nevenmitrovic4@gmail.com>
2025-08-28 11:10:53 +0200

    chore: cache with next revalidate config

--- caching-project/app/messages/page.js
+++ caching-project/app/messages/page.js
@@ -1,16 +1,16 @@
-import Messages from '@/components/messages';
+import Messages from '@/components/messages'
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
-  const messages = await response.json();
+	const response = await fetch('http://localhost:8080/messages', {
+		next: {
+			revalidate: 5,
+		},
+	})
+	const messages = await response.json()
 
-  if (!messages || messages.length === 0) {
-    return <p>No messages found</p>;
-  }
+	if (!messages || messages.length === 0) {
+		return <p>No messages found</p>
+	}
 
-  return <Messages messages={messages} />;
+	return <Messages messages={messages} />
 }
